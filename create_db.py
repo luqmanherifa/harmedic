39d78c2a901facfd40504a9a639a957cab8b4bdb
Luqman Herifa <luqmanherifa@gmail.com>
2025-06-26 13:20:52 +0700

    Create table users

---
+++ create_db.py
@@ -0,0 +1,10 @@
+# create_db.py
+from app import create_app, db
+from app.models import User
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+    print("Tabel berhasil dibuat di database.")
+
