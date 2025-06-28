374ee02833570a2cc19f97c4b59de24a79d0bd5e
Luqman Herifa <luqmanherifa@gmail.com>
2025-06-28 10:49:42 +0700

    Enhance post management UI

--- app/static/js/dom.js
+++ app/static/js/dom.js
@@ -32,30 +32,44 @@ const dom = {
     const rows = posts
       .map(
         (post) => `
-    <tr>
-      <td class="border px-4 py-2">${post.id}</td>
-      <td class="border px-4 py-2">${post.title}</td>
-      <td class="border px-4 py-2">${post.content}</td>
-      <td class="border px-4 py-2 space-x-1">
-        <button class="detail-btn bg-indigo-600 text-white px-2 py-1 rounded" 
-                data-id="${post.id}" 
-                data-title="${post.title}" 
-                data-content="${post.content}">
-          Detail
-        </button>
-        <button class="edit-btn bg-yellow-500 text-white px-2 py-1 rounded" 
-                data-id="${post.id}" 
-                data-title="${post.title}" 
-                data-content="${post.content}">
-          Edit
-        </button>
-        <button class="delete-btn bg-red-600 text-white px-2 py-1 rounded" 
-                data-id="${post.id}">
-          Delete
-        </button>
-      </td>
-    </tr>
-  `
+          <tr
+            class="bg-white border-b border-gray-200 hover:bg-gray-50"
+            >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+            >
+              ${post.id}
+            </th>
+            <td class="px-6 py-4">${post.title}</td>
+            <td class="px-6 py-4">${post.content}</td>
+            <td class="px-0 py-4">
+              <button
+                class="detail-btn font-medium text-blue-600 hover:underline"
+                  data-id="${post.id}" 
+                  data-title="${post.title}" 
+                  data-content="${post.content}"
+                >Detail</button
+              >
+            </td>
+            <td class="px-0 py-4">
+              <button
+                class="edit-btn font-medium text-yellow-600 hover:underline"
+                  data-id="${post.id}" 
+                  data-title="${post.title}" 
+                  data-content="${post.content}"
+                >Edit</button
+              >
+            </td>
+            <td class="px-0 py-4">
+              <button
+                class="delete-btn font-medium text-red-600 hover:underline"
+                  data-id="${post.id}"
+                >Delete</button
+              >
+            </td>
+          </tr>
+        `
       )
       .join("");
 
